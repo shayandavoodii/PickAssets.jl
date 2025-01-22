a90fe9206e4bbf7c5966ea15964c9fc883991181
Shayan <sh0davoodi@gmail.com>
2025-01-22 18:21:11 +0330

    [src/Types.jl] Types evolved 🔧

--- src/Types.jl
+++ src/Types.jl
@@ -1,10 +1,22 @@
 abstract type Method end
 abstract type Partition end
+abstract type Span end
 
 struct HighVolatility <: Method end
 struct Random <: Method end
+struct DateBased{T<:Span} <: Partition
+  span::T
+end
 
-struct YearlyPartitioned <: Partition end
-struct SeasonalyPartitioned <: Partition end
-struct MonthlyPartitioned <: Partition end
-struct DailyPartitioned <: Partition end
+struct ValueBased{T<:Span} <: Partition
+  span::T
+end
+
+@kwdef struct Yearly{T<:Int} <: Span
+  val::T=252
+end
+@kwdef struct Monthly{T<:Int} <: Span
+  val::T=21
+end
+
+struct Seasonaly <: Span end

3bcce3ebcbafaa55d0537338cecf136ba53d56f8
Shayan <sh0davoodi@gmail.com>
2025-02-07 09:23:56 +0330

    [ext/YFPickAssets.jl] Major bug fixed 🐛

--- ext/YFPickAssets.jl
+++ ext/YFPickAssets.jl
@@ -23,7 +23,14 @@ function PickAssets.pickassets(m::PickAssets.MarketCap, tickers::AbstractVector{
   than or equal to the number of tickers."))
   caps::Vector{Int} = map(x->x["marketCap"], get_summary_detail.(tickers))
   meancaps = PickAssets._mean(caps)
-  idxs = findall(caps.≥meancaps)[1:m.n]
+  idxs = findall(caps.≥meancaps)
+  if length(idxs) < m.n
+    @warn "The number of assets to pick is greater than the number of tickers with a market \
+    cap greater than the mean. The most possible number of assets ($(length(idxs))) will be picked."
+    idxs = idxs[1:length(idxs)]
+  else
+    idxs = idxs[1:m.n]
+  end
   res = Dict(tickers[i] => float(caps[i]) for i=eachindex(caps))
   return PickAssets.PickedAssets(meancaps, tickers[idxs], idxs, res)
 end

6e64be2affe5bf5714dddf207c25f7ccae2b1645
Shayan <sh0davoodi@gmail.com>
2025-02-07 09:25:44 +0330

    [test/ext.jl] Extension unit tests provided 🧪

---
+++ test/ext.jl
@@ -0,0 +1,17 @@
+@testset "YFPickAssets.jl" begin
+  mc = MarketCap(3)
+  r = pickassets(mc, tickers)
+  @test length(r.idx) ≤ mc.n
+  @test r isa PickAssets.PickedAssets
+  @test_throws ArgumentError pickassets(mc, tickers[1:2])
+
+  mc = MarketCap(3)
+  r = pickassets(mc, tickers[1:3])
+  @test length(r.idx) < mc.n
+  @test r isa PickAssets.PickedAssets
+
+  mc = MarketCap(2)
+  r = pickassets(mc, tickers)
+  @test length(r.idx) == mc.n
+  @test r isa PickAssets.PickedAssets
+end

350d814d1c045a0f5c89b7cab28e38a17a6b5bd4
Shayan <sh0davoodi@gmail.com>
2025-02-17 22:48:50 +0330

    [ext.jl] Further unit tests for `pickassets!` provided 🧪

--- test/ext.jl
+++ test/ext.jl
@@ -14,4 +14,23 @@
   r = pickassets!(mc, tickers)
   @test length(r.idx) == mc.n
   @test r isa PickAssets.PickedAssets
+
+  tickers_ = ["689009.SS", "MSFT", "TSLA", "AAPL"]
+  @test_logs (:warn, r"There is no market cap recorded for \[\"689009.SS\"\]. These tickers will get deleted from the `tickers` Vector.") pickassets!(mc, tickers_)
+  tickers_ = ["689009.SS", "MSFT", "TSLA", "AAPL"]
+  mc = MarketCap(2)
+  r = pickassets!(mc, tickers_)
+  @test length(r.idx) == mc.n
+  @test r isa PickAssets.PickedAssets
+  @test tickers_ == ["MSFT", "TSLA", "AAPL"]
+  @test length(r.idx) == 2
+
+  tickers_ = ["MSFT", "689009.SS"]
+  mc = MarketCap(2)
+  r = pickassets!(mc, tickers_)
+  @test tickers_ == ["MSFT"]
+  @test length(r.idx) == 1
+  @test r.idx == [1]
+  @test r.sup == ["MSFT"]
+
 end
